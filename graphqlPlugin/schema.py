8070e7385905799f3e7170b35e95013fa29b315e
narendersaini32 <narendersaini32@gmail.com>
2023-04-21 09:08:02 +0530

    3. Added email verification API logic using the graphql. `Done`

--- graphqlPlugin/schema.py
+++ graphqlPlugin/schema.py
@@ -1,10 +1,11 @@
 from graphene import ObjectType, String, Schema
-from flowback.user.services import user_create
+from flowback.user.services import user_create,user_create_verify
 
 class Query(ObjectType):
     # this defines a Field `hello` in our Schema with a single Argument `first_name`
     # By default, the argument name will automatically be camel-based into firstName in the generated schema
     register = String(username=String(),email=String())
+    registerVerify = String(verificationCode=String(),password=String())
     
     # Call the function with the provided arguments
     # our Resolver method takes the GraphQL context (root, info) as well as
@@ -12,6 +13,9 @@ class Query(ObjectType):
     def resolve_register(root, info, username,email):
         user_create(username=username, email=email)
         return "Registered"
-   
+
+    def resolve_registerVerify(root, info, verificationCode,password):
+        user_create_verify(verification_code=verificationCode, password=password)
+        return "Verified Successfully"
 
 schema = Schema(query=Query)
