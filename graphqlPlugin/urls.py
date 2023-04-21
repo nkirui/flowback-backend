cb11aae47492946f9b540d47f33883046abfcaf0
narendersaini32 <narendersaini32@gmail.com>
2023-04-21 08:44:25 +0530

    1. Graphql endpoints path updated. `Done`

--- graphqlPlugin/urls.py
+++ graphqlPlugin/urls.py
@@ -3,5 +3,5 @@ from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('test', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
