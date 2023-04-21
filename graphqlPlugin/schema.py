1b50615c26a02cb454a1ae163b2071c73b2b9005
narendersaini32 <narendersaini32@gmail.com>
2023-04-21 08:58:49 +0530

    2. Added code to call the register API logic using graphql. `Done`

--- graphqlPlugin/schema.py
+++ graphqlPlugin/schema.py
@@ -1,15 +1,17 @@
 from graphene import ObjectType, String, Schema
+from flowback.user.services import user_create
 
 class Query(ObjectType):
     # this defines a Field `hello` in our Schema with a single Argument `first_name`
     # By default, the argument name will automatically be camel-based into firstName in the generated schema
     register = String(username=String(),email=String())
-
+    
+    # Call the function with the provided arguments
     # our Resolver method takes the GraphQL context (root, info) as well as
     # Argument (first_name) for the Field and returns data for the query Response
     def resolve_register(root, info, username,email):
-        return f'{username} {email}!'
-
+        user_create(username=username, email=email)
+        return "Registered"
    
 
 schema = Schema(query=Query)
