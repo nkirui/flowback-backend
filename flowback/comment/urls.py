e8fa95665f4677e846603b0de2fcc65de12f4eb9
Nathan Kirui <nathankirui5@gmail.com>
2024-05-29 12:48:21 +0300

    revert change as the comments views are supposed to be used elsewhere and not directly

--- flowback/comment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-comment_patterns = [
-    # we need have all urls to CRUD the comments
-    path("<int:comment_section_id>", views.CommentListAPI.as_view(), name="comment_list"),
-    path("<int:comment_section_id>/<int:comment_id>", views.CommentDetailAPI.as_view(), name="comment_detail"),
-]
